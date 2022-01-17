d2feb0cd51dad5454e1f6c301e417563bfbfc27c
AdityaIT21 <96476549+AdityaIT21@users.noreply.github.com>
2022-01-17 21:05:58 +0530

    Update db.php

--- admin/functions/db.php
+++ admin/functions/db.php
@@ -3,10 +3,10 @@
   /* DATABASE CONNECTION*/
 
 
-       $db['db_host'] = 'localhost';
-        $db['db_user'] = 'root';
-        $db['db_pass'] = '';
-        $db['db_name'] = 'idea';
+       $db['db_host'] = '127.0.0.1:3306';
+        $db['db_user'] = 'u105808218_ideashacks';
+        $db['db_pass'] = 'Ideashacks@2022';
+        $db['db_name'] = 'u105808218_ideashacks';
 
       foreach($db as $key=>$value){
           define(strtoupper($key),$value);
@@ -18,7 +18,7 @@
       }
 
       try{
-             $db = new PDO('mysql:dbhost=localhost;dbname=idea;charset=utf8','root','');
+             $db = new PDO('mysql:dbhost=127.0.0.1:3306;dbname=u105808218_ideashacks;charset=utf8','u105808218_ideashacks','Ideashacks@2022');
 
       }
       catch(Exception $e){
